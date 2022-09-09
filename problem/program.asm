bcf489ab4fbaf61b9b130b905a27d98215ed91ba
Juaandangel <juaandangel99@gmail.com>
2022-09-08 22:36:12 -0500

    Minor Changes

--- problem/program.asm
+++ problem/program.asm
@@ -5,33 +5,26 @@
 (START)
             @16400
             D=A
-
             @pscreen
             M=D
-
             @256
             D=A
-
             @contador
             M=D
-
             @32
             D=A
             @contador2
             M=D
-
             @4096
             D=A
             @inicio
             M=D
-
             @KBD
             D=M
             @84
             D=D-A
             @LVER
             D;JEQ
-
             @KBD
             D=M
             @67
@@ -41,16 +34,31 @@
 
             @START
             0;JMP
+
+
+(LHOR)
+             @inicio
+             D=M
+             @SCREEN
+             A=A+D
+             M = -1
+             @inicio
+             M=M+1
+             @contador2
+             M=M-1
+             D=M
+             @END
+             D;JEQ
+            @LHOR
+            0;JMP
+
 (LVER)
             @contador
             D=M
-
             @LHOR
             D;JEQ
-
             @256
             D=A
-
             @pscreen
             A=M
             M=D
@@ -60,28 +68,7 @@
             D=A
             @pscreen
             M=M+D
-
             @LVER
-            0;JMP
-
-(LHOR)
-             @inicio
-             D=M
-
-             @SCREEN
-             A=A+D
-             M = -1
-
-             @inicio
-             M=M+1
-
-             @contador2
-             M=M-1
-             D=M
-             @END
-             D;JEQ
-
-            @LHOR
             0;JMP
 
 (REGIS)
@@ -111,27 +98,19 @@
 (PAINT)
             @cont
             D=M
-
             @END
             D;JEQ
-
-
-
             @color
             D=M
             @pscreen
             A=M
             M=D
-
             @cont
             M=M-1
-
             @jump
             D=M
-
             @pscreen
             M=M+D
-
             @PAINT
             0;JMP
 
